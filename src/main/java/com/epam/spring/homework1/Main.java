341a5411bd9585c9b524b9c814c9fa17522cfad9
Olena Solodiankina <alyona.solodiankina@gmail.com>
2022-01-19 23:55:36 +0200

    HW-1 - add more options with @Primary and @Qualifier annotations
    Resolves: task item 10, the last one.

--- src/main/java/com/epam/spring/homework1/Main.java
+++ src/main/java/com/epam/spring/homework1/Main.java
@@ -1,6 +1,8 @@
 package com.epam.spring.homework1;
 
 import com.epam.spring.homework1.config.BeanConfig;
+import com.epam.spring.homework1.other.OtherBeanC;
+import com.epam.spring.homework1.pet.Cheetah;
 import com.epam.spring.homework1.pet.Pet;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -11,5 +13,16 @@ public class Main {
 
         Pet pet = ctx.getBean(Pet.class);
         pet.printPets();
+
+        // item 10
+        // this uses @Primary bean
+        Cheetah cheetah = ctx.getBean(Cheetah.class);
+        System.out.println("\n" + cheetah.hashCode());
+
+        // this uses method with name myCheetah from PetConfig
+        Cheetah cheetah2 = (Cheetah) ctx.getBean("myCheetah");
+        System.out.println(cheetah2.hashCode());
+
+        // and they are different objects (see hashCode) because of different ways of calling them
     }
 }
